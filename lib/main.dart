fef292915eb87edf394ecff25e5cc7c06ed9a7e8
Sanskar <sanskar4singh6@gmail.com>
2024-05-14 01:45:21 +0530

    File name change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Login Page',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,13 +31,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Loginpage(title: 'Flutter Login Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -51,10 +51,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Loginpage> createState() => _LoginpageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LoginpageState extends State<Loginpage> {
   int _counter = 0;
 
   void _incrementCounter() {
